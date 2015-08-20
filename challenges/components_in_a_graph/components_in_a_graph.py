757e977fc840681333d5d25bafada3ca1465a463
Geison Goes <geison.goes@hp.com>
2015-08-20 15:40:02 -0300

    Disjoint Set Problems 2

--- challenges/components_in_a_graph/components_in_a_graph.py
+++ challenges/components_in_a_graph/components_in_a_graph.py
@@ -1,3 +1,52 @@
+"""
+Problem Statement
+
+There are 2N values to represent nodes in a graph. They are divided into two
+sets G and B. Each set has exactly N values. Set G is represent by
+{G1,G2,...,GN}. G can take any value between 1 to N(inclusive). Set B is
+represented by {B1,B2,⋯,BN}. B can take any value between N+1 to
+2N(inclusive). Same value can be chosen any number of times.
+
+Here (G1,B1),(G2,B2),...(GN,BN) represents the edges of the graph.
+
+Your task is to print the number of vertices in the smallest and the largest
+connected components of the graph.
+
+Input Format
+
+First line contains an integer N. 
+Each of the next N lines contain two space-separated integers, ith line
+contains Gi and Bi.
+
+Constraints 
+
+1<=N<=15000 
+1<=Gi<=N 
+N+1<=Bi<=2N
+
+Output Format
+
+Print two space separated integers, the number of vertices in the smallest
+and the largest components.
+
+Sample Input
+5
+1 6 
+2 7
+3 8
+4 9
+2 6
+
+Sample Output
+2 4
+
+Explanation
+The number of vertices in the smallest connected component in the graph is 2
+i.e. either (3,8) or (4,9). 
+The number of vertices in the largest connected component in the graph is 4
+i.e. 1−2−6−7.
+"""
+
 class UnionFind(object):
 
     def __init__(self, n):
